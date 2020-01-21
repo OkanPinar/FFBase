92b06886311ac37454195ab52c8822a7eb9463d8
Okan Pınar <33915699+mortarfemman@users.noreply.github.com>
2020-01-21 15:00:45 +0300

    Computer Change

---
+++ FFBase/FFUICommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace FFBase
+{
+    public interface ICommandable
+    {
+        Dictionary<string, FFUICommand> Commands { get; set; }
+    }
+
+    public static class FFUICommandStack
+    {
+
+    }
+
+    public class FFUICommand: ICommand
+    {
+        public object Source { get; set; }
+
+        public Action Redo { get; set; }
+
+        public Action Undo { get; set; }
+
+        public string CommandInfo { get; set; }
+
+        public FFUICommand()
+        {
+
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
